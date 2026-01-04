72555b196b43c04e1236653a2164c2203ce380d0
AlsoMeParth <opxplays@gmail.com>
2026-01-04 08:09:20 +0530

    adding custom confidence scores for the 3 categories

--- scripts/detect_live.py
+++ scripts/detect_live.py
@@ -14,6 +14,12 @@ if not cap.isOpened():
 
 print("Press 'q' to quit the application")
 
+# Custom confidence score for each class
+custom_thresholds = {
+    0: 0.70,  # Strict for Mask
+    1: 0.65,  # Moderate for No Mask
+    2: 0.45   # Lenient for Incorrect (harder to detect)
+}
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -21,12 +27,17 @@ while True:
 
     # 3. Using model on each frame
     # imgsz=640 matches training size
-    results = model(frame, imgsz=640, conf=0.8)
-
+    results = model(frame, imgsz=640, conf=0.25)
+    for r in results:
+        keep_indices = []
+        for i, conf in enumerate(r.boxes.conf):
+            cls_id = int(r.boxes.cls[i])
+            if conf >= custom_thresholds.get(cls_id, 0.5):
+                keep_indices.append(i)
+        r.boxes = r.boxes[keep_indices]
     # 4. Visualizing results on the frame
     # .plot() function comes in ultralytics library and replaces open CV code for drawing rectangle on the frame with respective coordinates.
-    annotated_frame = results[0].plot()
-                                
+        annotated_frame = r.plot()                      
     # 5. Result
     cv2.imshow('Face Mask Detector - Live', annotated_frame)
 
